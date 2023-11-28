7c3a4daedcc84c0080909c4ba6cca8c8d5cc993c
Antonio Iñiguez <antonioiniher@icloud.com>
2023-11-28 19:46:51 +0100

    0

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,8 @@ module.exports = app => {
 
     const ClassRoutes = require("./class.routes")
     app.use("/api/class", ClassRoutes)
+
+    const UploadRoutes = require("./upload.routes")
+    app.use("/api/upload", UploadRoutes)
+
 }
